e1cd88cf92ce22ea2b16d5aa27c573270cdd7e19
gbenga omoni <gbengaomoni@gmail.com>
2020-07-30 19:51:12 -0600

    Adde google api importer

--- pages/api/mobile/importmembers.js
+++ pages/api/mobile/importmembers.js
@@ -10,29 +10,24 @@ handler.get(async (req, res) => {
   try {
     let result = { status: false, data: null, error: "Not found (X160)" };
     let members = {};
-    let doc = await req.db
-      .collection("members")
-      .findOne({ firebaseId: req.query.firebaseid });
 
-    if (doc) {
-      members = await req.db
-        .collection("members")
-        .find(
-          {},
-          {
-            sort: { lastName: 1 },
-          }
-        )
-        .toArray();
-      result = { status: true, data: members, error: null };
-    }
+    members = await req.db
+      .collection("members")
+      .find(
+        {},
+        {
+          sort: { lastName: 1 },
+        }
+      )
+      .toArray();
+    result = { status: true, data: members, error: null };
 
     res.json(result);
   } catch (exception) {
     //debugger;
     slack.send({
       channel: "#fak94Errors",
-      text: `email : ${req.query.email} \n firebaseid : ${req.query.firebaseid} \n  ${exception.stack}`,
+      text: `email : ${req.query.email} \n  ${exception.stack}`,
     });
     res.json({ status: false, data: null, error: "An Error occurred (X300)" });
   }
