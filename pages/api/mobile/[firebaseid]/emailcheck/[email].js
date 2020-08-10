833d0792368f91ee9a085947b6044c12f31b8aa1
gbenga omoni <gbengaomoni@gmail.com>
2020-08-09 22:00:20 -0600

    Added email check

---
+++ pages/api/mobile/[firebaseid]/emailcheck/[email].js
@@ -0,0 +1,50 @@
+import nextConnect from "next-connect";
+import { ObjectId } from "mongodb";
+import middleware from "../../../../middleware/database";
+import slack from "../../../../middleware/logger";
+
+const handler = nextConnect();
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  try {
+    let result = { status: false, data: null, error: "Not found (X060)" };
+    let doc = await req.db
+      .collection("members")
+      .findOne({ email: req.query.email });
+
+    if (doc && !doc.firebaseId) {
+      await req.db.collection("members").updateOne(
+        { email: req.query.email },
+        {
+          $set: {
+            firebaseId: req.query.firebaseid,
+          },
+        }
+      );
+      result = { status: true, data: doc, error: null };
+    }
+
+    if (doc && doc.firebaseId && req.query.firebaseid != doc.firebaseId) {
+      console.log(req.query.firebaseid);
+      console.log(doc.firebaseId);
+      result = {
+        status: false,
+        data: null,
+        error: "Data mismatch getting user data (X070)",
+      };
+    } else {
+      result = { status: true, data: doc, error: null };
+    }
+    res.json(result);
+  } catch (exception) {
+    //debugger;
+    slack.send({
+      channel: "#fak94Errors",
+      text: `email : ${req.query.email} \n firebaseid : ${req.query.firebaseid} \n  ${exception.stack}`,
+    });
+    res.json({ status: false, data: null, error: "An Error occurred (X200)" });
+  }
+});
+
+export default handler;
